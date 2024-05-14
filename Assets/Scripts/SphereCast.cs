ed21fd684b7070f2efd7d05a81028756b55fd032
Sined0r <funfoxi4@mail.ru>
2024-05-14 19:41:58 +0300

    чуть скриптов

---
+++ Assets/Scripts/SphereCast.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCast : MonoBehaviour
+{
+    [SerializeField] AudioClip _audio;
+
+    void Update()
+    {
+        if (_audio)
+        {
+
+        }
+    }
+
+    public void start ()
+    {
+
+    }
+}
